59c1d5c256d3c94bd626e46b55a33b50a5a66b89
c2044520 <oliverplug@hotmail.com>
2022-11-03 15:26:02 +0000

    client class added

--- bank of plugz/main.py
+++ bank of plugz/main.py
@@ -1,3 +1,19 @@
 class client:
+# File name = main.py
+    def __init__(self, customer_id, title, first_name, last_name,
+                 birth_date, gender, email, occupation, account_balance, overdraft_limit):
+        self.customer_id = customer_id
+        self.title = title
+        self.first_name = first_name
+        self.last_name = last_name
+        self.birth_date = birth_date
+        self.gender = gender
+        self.email = email
+        self.occupation = occupation
+        self.account_balance = account_balance
+        self.overdraft_limit = overdraft_limit
 
-    def __init__(self, customer_id,):
+
+
+
+
