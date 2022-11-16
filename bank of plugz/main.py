7ec2640a459fed64545205e238fdab532488fbea
c2044520 <oliverplug@hotmail.com>
2022-11-16 01:15:50 +0000

    addition of functions

--- bank of plugz/main.py
+++ bank of plugz/main.py
@@ -1,36 +1,58 @@
-customer = input("welcome to the bank of plug" 
-                "\nClick 1 to withdraw money"
-                "\n click 2 to add money to the account"
-                "\n click 3 to quit\n")
-if customer =="1":
-    balance = int(input("your balance here"))
-    withdraw = int(input("how much would you like to withdraw"))
-    if balance < withdraw:
-        print("account overdraft balance", (balance - withdraw))
-    else:
-        print("your new balance:", (balance - withdraw))
-
 import csv
-
+print("welcome")
+user_id = range(1, 100, 1,)
+id = int(input("please enter user id"))
+customer = input("welcome to the bank of plug"
+                 "\n to see details type show_details()"
+                 "\n to add funds type add_funds()"
+                 "\n to withdraw type withdraw()"
+                 "\n to see account balance type see_balance()\n")
+#parent class
 class client:
-# File name = main.py
-    def __init__(self, customer_id, title, first_name, last_name,
-                 birth_date, gender, email, occupation, account_balance, overdraft_limit):
+    def __init__(self, customer_id, first_name, last_name,
+                 birth_date, gender,):
         self.customer_id = customer_id
-        self.title = title
         self.first_name = first_name
         self.last_name = last_name
         self.birth_date = birth_date
         self.gender = gender
-        self.email = email
-        self.occupation = occupation
-        self.account_balance = account_balance
-        self.overdraft_limit = overdraft_limit
+
+    def show_details(self):
+        print("client details")
+        print("id ",self.customer_id)
+        print("name ",self.first_name + self.last_name)
+        print("birth date ",self.birth_date)
+        print("gender ",self.gender)
+#child class
+class bank:
+    def __innit__(self, customer_id, first_name, last_name,
+                  birth_date, gender,):
+        super().__init__(customer_id, first_name, last_name,
+                         birth_date, gender,)
+        self.balance = 0
+
+    def add_funds(self,amount):
+        self.balance = self.balance + self.amount
+        print("current account balance  : £", self.balance)
+    def withdraw(self,amount):
+        self.amount = amount
+        if self.amount > self.balance:
+            print("you have entered your overdraft current balance is",
+                  self.balance - self.amount)
+        else:
+            self.balance - self.amount
+            print("balance has been updated new balance is",
+                  self.balance)
+    def see_balance(self):
+        self.show_details()
+        print("current account balance  : £", self.balance)
+
+
 
 def read(self):
 
 
-    filename = "client_data.csv"
+    filename = "clients.csv"
     rows = []
 
     with open(filename, 'r') as csvfile:
